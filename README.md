f9a3df671cc8040ba11c3737902a8bb0aa73875a
Michael <mdimovich@gmail.com>
2023-06-03 14:27:11 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# rusty md5: md5 message digest algorithm implemented using rust
+
+### description
+This is a from scratch implementation of the md5 message digest algorithm using
+rust. 
+
+The purpose of this code is to act as a learning resource
+for the md5 message digest algorithm. I wanted to provide
+a straightforward walk-through of the md5 algorithm using
+Rust.
+
+This code was implemented using the original md5 message digest
+RFC, written by R. Rivest iin April 1992.
+
+For more information about the md5 algorithm, or the original C
+source code implementation, please reference RFC 1321.
+
+*(source: https://datatracker.ietf.org/doc/html/rfc1321)*
+
+
+### to build / run locally
+1. download the repo
+2. run `cargo build` inside the project
+3. run `cargo run <string_to_hash>` to get a md5 
+   message digest of a string
+4. run `cargo test` to run unit tests
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 /**
 *
-* The MD5 Message-Digest Algorithm, implemented in Rust.
-* implemented by michael dimovich (@mdimovich)
+* rusty_md5
+*
+* The MD5 Message-Digest Algorithm, written in Rust.
+* Implemented by michael dimovich (@mdimovich)
 *
 * The purpose of this code is to act as a learning resource
 * for the md5 message digest algorithm. I wanted to provide
@@ -16,19 +18,6 @@
 *
 * (source: https://datatracker.ietf.org/doc/html/rfc1321)
 *
-*/
-
-/*
-MD5 Test Suite From RFC Implementation
-MD5 ("") = d41d8cd98f00b204e9800998ecf8427e
-MD5 ("a") = 0cc175b9c0f1b6a831c399e269772661
-MD5 ("abc") = 900150983cd24fb0d6963f7d28e17f72
-MD5 ("message digest") = f96b697d7cb7938d525a2f31aaf161d0
-MD5 ("abcdefghijklmnopqrstuvwxyz") = c3fcd3d76192e4007dfb496cca67e13b
-MD5 ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") =
-d174ab98d277d9f5a5611c2c9f419d9f
-MD5 ("123456789012345678901234567890123456789012345678901234567890123456
-78901234567890") = 57edf4a22be3c955ac49da2e2107b67a
 */
 
 fn remove_first(vec: &mut Vec<String>) -> Option<String> {
