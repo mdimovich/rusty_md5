1d503da90197e3c49f17d7775fe010fd10bfa8c2
Michael <mdimovich@gmail.com>
2023-06-03 14:59:12 -0400

    fix some comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::convert::TryInto;
 /**
 *
-* The MD5 Message-Digest Algorithm, implemented in Rust.
-* implemented by michael dimovich (@mdimovich)
+* The MD5 Message-Digest Algorithm, written in Rust.
+* Implemented by michael dimovich (@mdimovich)
 *
 * The purpose of this code is to act as a learning resource
 * for the md5 message digest algorithm. I wanted to provide
@@ -10,7 +10,7 @@ use std::convert::TryInto;
 * Rust.
 *
 * This code was implemented using the original md5 message digest
-* RFC, written by R. Rivest iin April 1992.
+* RFC, written by R. Rivest in April 1992.
 *
 * For more information about the md5 algorithm, or the original C
 * source code implementation, please reference RFC 1321.
